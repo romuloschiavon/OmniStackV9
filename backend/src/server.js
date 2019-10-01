8b22b380cefd2c6dd2af86729b24b3af0979c624
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-01 11:43:53 -0300

    Created MVC

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,12 @@
 const express = require('express'); //Requiring the main dependency Express
+const mongoose = require('mongoose'); //Require mongoose
 const app = express(); //Defining our app!
+const routes = require('./routes'); //Requiring our routes
+
+mongoose.connect('mongodb+srv://omnistacker:omnistacker@omnistackv9-tzs9q.mongodb.net/Week09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // GET; POST; PUT; DELETE
 
@@ -10,6 +17,7 @@ req.body = access body of the requisition for everything
 */
 
 app.use(express.json());
+app.use(routes);
 
 
 app.listen(3333);
