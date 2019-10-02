a0d62fd475bfed7944b4f7e9ab3d3569fc92ce21
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-02 00:02:54 -0300

    Spot Controller

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,9 +1,18 @@
-const Spot = require('../models/Spot');
+const Spot = require('../models/Spot'); //Require spot models
 
 module.exports = {
     async store(req, res) {
         const { filename } = req.file;
         const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+        })
 
 
         return res.json({ ok: true});
