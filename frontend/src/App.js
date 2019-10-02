67d18095f5f657ed21f6326d470586891a41a7f9
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-02 17:10:00 -0300

    Modularing app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import api from './services/api';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState('');
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    
-    const response = await api.post('/sessions', { email });
-    
-    const { _id } = response.data;
-
-    console.log(_id);
-  }
 
   return (
     <div className="container">
       <img src={logo} alt="AirCnCLogo"/>
 
       <div className="content">
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-mail *</label>
-          <input 
-            id="email" 
-            type="email" 
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}>
-          </input>
-          <button 
-            type="submit"
-            className="btn">Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      
+      const response = await api.post('/sessions', { email });
+      
+      const { _id } = response.data;
+  
+      console.log(_id);
+    }
+
+    return (
+    <>
+        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail *</label>
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}>
+          </input>
+          <button 
+            type="submit"
+            className="btn">Entrar
+          </button>
+        </form>
+    </>
+    )
+}
