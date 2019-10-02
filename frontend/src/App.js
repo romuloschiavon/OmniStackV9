20b175eee19df039726e34e42fcc160cbee0f463
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-02 11:48:12 -0300

    Creating email route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import api from './services/api';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email);
+
+
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnCLogo"/>
@@ -11,12 +21,14 @@ function App() {
       <div className="content">
         <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail *</label>
           <input 
             id="email" 
             type="email" 
-            placeholder="Seu melhor e-mail">
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}>
           </input>
           <button 
             type="submit"
