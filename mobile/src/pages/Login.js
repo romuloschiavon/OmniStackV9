2f1050ebeb6dd25690a0da594855efb49c8e1141
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-03 11:28:33 -0300

    Houston we have bugs!

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,41 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login() {
-    return <View/>
+    return (
+        <View style={styles.container}>
+            <Image source={logo} />
+
+            <View style={styles.form}>
+                <Text style={styles.label}>Your E-mail *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Your e-mail"
+                    placeholderTextColor='#999'
+                />
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+         alignItems: 'center'
+    },
+
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8
+    }
+});
