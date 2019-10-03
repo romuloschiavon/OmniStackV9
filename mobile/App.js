91edafaa7704e5c643f4dcfa677d54e35c4c2f91
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-03 11:06:31 -0300

    Mobile dev start!

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Routes from './src/routes';
+
+//View = Div
+//Text = simple text without any style
+//StyleSheet = create a .css inside a JS file
+
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
