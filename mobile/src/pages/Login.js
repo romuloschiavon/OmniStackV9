04f4d5378186513e17ca6987b922a0b2ee8e915d
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-03 12:06:13 -0300

    BackEnd had some issues, now getting the ID via APP is working fine

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,22 +1,58 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform, View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
 export default function Login() {
+    const [ email, setEmail ] = useState('');
+    const [ techs, setTechs ] = useState('');
+    
+    async function handleSubmit() {
+        // email, techs
+        const response = await api.post('/sessions', {
+            email
+        });
+
+        const { _id } = response.data;
+
+        console.log(_id);
+    }
+
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior='padding' style={styles.container}>
             <Image source={logo} />
 
             <View style={styles.form}>
-                <Text style={styles.label}>Your E-mail *</Text>
+                <Text style={styles.label}>E-mail *</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="Your e-mail"
                     placeholderTextColor='#999'
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={email}
+                    onChangeText={setEmail}
                 />
+                
+                <Text style={styles.label}>Techs *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Technologies of interest"
+                    placeholderTextColor='#999'
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
+                />
+
+                <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                    <Text style={styles.buttonText}>Find spot</Text>
+                </TouchableOpacity>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -37,5 +73,30 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#444',
         marginBottom: 8
-    }
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2,
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
 });
