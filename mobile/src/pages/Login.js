eacb93c1ff3e3d4fd706e430b7581917d9b3c7e6
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-03 14:34:39 -0300

    Almost finished

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
-import { KeyboardAvoidingView, Platform, View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { KeyboardAvoidingView, AsyncStorage, Platform, View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login( { navigation }) {
     const [ email, setEmail ] = useState('');
     const [ techs, setTechs ] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('List');
+            }
+        })
+    }, []);
     
     async function handleSubmit() {
         // email, techs
@@ -17,7 +25,10 @@ export default function Login() {
 
         const { _id } = response.data;
 
-        console.log(_id);
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
     }
 
     return (
