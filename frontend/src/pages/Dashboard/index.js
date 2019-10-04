0e43b9ecd5a554fc0485ffe3c3c2367df80eafff
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-10-04 10:02:49 -0300

    So right now, all the routes have access to our SocketID and the ConnectedUsers

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
+
 import api from '../../services/api';
 import './styles.css';
 
 export default function Dashboard() {
-    const [spots, setSpots] = useState([]);
-    
-    useEffect(() => {
+    const [spots, setSpots] = useState([]); //Defining our useState for spots
+    useEffect(() => { //UseEffect that only eexecutes one time, it is, when the front end login successfully
+        const user_id = localStorage.getItem('user');
+        const socket = socketio('http://localhost:3333', {
+            query: { user_id } //Getting our user Id from our route
+        });
+    }, []);
+
+
+    useEffect(() => { //useEffect for loading spots. Using our user_id!
         async function loadSpots() {
             const user_id = localStorage.getItem('user');
             const response = await api.get('/dashboard', {
@@ -20,6 +29,7 @@ export default function Dashboard() {
     }, []);
     
     return (
+        //Our return is indeed a HTML, with a unique key for each spot
         <>
             <ul className="spot-list">
                 {spots.map(spot => (
